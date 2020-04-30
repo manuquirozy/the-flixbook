5fda0de9ade84b15dbe213d316417bdc8ba36f6c
Manuela Quiroz <manuquirozy@gmail.com>
2020-04-29 22:34:00 -0500

    Updates

---
+++ precache-manifest.086fd3e66418d96f8ccdaeeb647be292.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ea61d7246549740439274ae98f396de",
+    "url": "/the-flixbook/index.html"
+  },
+  {
+    "revision": "29d2e41178a531a20ebd",
+    "url": "/the-flixbook/static/css/main.be18f5a4.chunk.css"
+  },
+  {
+    "revision": "72aaadc953a8b8345dad",
+    "url": "/the-flixbook/static/js/2.4fa38ef2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/the-flixbook/static/js/2.4fa38ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29d2e41178a531a20ebd",
+    "url": "/the-flixbook/static/js/main.bdd8ca22.chunk.js"
+  },
+  {
+    "revision": "a48a58b5c97cd996d2c4",
+    "url": "/the-flixbook/static/js/runtime-main.d4268789.js"
+  },
+  {
+    "revision": "4e897b056ebb42f3d77933192182b661",
+    "url": "/the-flixbook/static/media/logo.4e897b05.png"
+  },
+  {
+    "revision": "31d26327470d3d7f96f7bed3417fe659",
+    "url": "/the-flixbook/static/media/logoTMDB.31d26327.svg"
+  }
+]);
